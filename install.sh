f4ebe61b06f8a8c3fc9b2845c82c8e8ffa2ffa6f
erjui <ksj970531@gmail.com>
2022-10-08 19:28:32 +0900

    install: Add symlink script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# zsh symlink
+ln -s zsh ~/.zsh
+ln -s zsh/zlogin ~/.zlogin
+ln -s zsh/zlogout ~/.zlogout
+ln -s zsh/zprofile ~/.zprofile
+ln -s zsh/zshrc ~/.zshrc
+
+# vim symlink
+ln -s vim ~/.vim
+ln -s vim/vimrc ~/.vimrc
+
+# tmux symlink
+ln -s tmux ~/.tmux
+ln -s tmux/tmux.conf ~/.tmux.conf
