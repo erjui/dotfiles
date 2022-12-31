7bac6b786e56481868927fb5b89ed95c8fc228c8
erjui <ksj970531@gmail.com>
2022-12-31 13:16:08 +0900

    install: Check whether dotfile already exists or not

--- install.sh
+++ install.sh
@@ -1,18 +1,36 @@
 #!/usr/bin/env bash
 
-# zsh symlink
-ln -s ~/dotfiles/zsh/zlogin ~/.zlogin
-ln -s ~/dotfiles/zsh/zlogout ~/.zlogout
-ln -s ~/dotfiles/zsh/zprofile ~/.zprofile
-ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
-ln -s ~/dotfiles/zsh/p10k.zsh ~/.p10k.zsh
+safe_link(){
+    local src=$1
+    local dest=$2
 
-ln -s ~/dotfiles/zsh/fzf ~/.fzf
-ln -s ~/dotfiles/zsh/fzf.zsh ~/.fzf.zsh
+    if [ -f $dest ]; then
+        read -p "$dest already exists. Override? [y/n] " -n 1 -r
+        echo
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            rm $dest
+            ln -s $src $dest
+        else
+            echo "Skip $dest"
+            return
+        fi
+    fi
+}
+
+
+# zsh symlink
+safe_link ~/dotfiles/zsh/zlogin ~/.zlogin
+safe_link ~/dotfiles/zsh/zlogout ~/.zlogout
+safe_link ~/dotfiles/zsh/zprofile ~/.zprofile
+safe_link ~/dotfiles/zsh/zshrc ~/.zshrc
+safe_link ~/dotfiles/zsh/p10k.zsh ~/.p10k.zsh
+
+safe_link ~/dotfiles/zsh/fzf ~/.fzf
+safe_link ~/dotfiles/zsh/fzf.zsh ~/.fzf.zsh
 
 # vim symlink
-ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+safe_link ~/dotfiles/vim/vimrc ~/.vimrc
 
 # tmux symlink
-ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+safe_link ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 
