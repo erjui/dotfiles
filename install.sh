27f8dfc575b19d20edcaa94cfa5f5dbf5d86b1dd
erjui <ksj970531@gmail.com>
2022-12-31 13:14:07 +0900

    install: Refactor install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # zsh symlink
-# ln -s ~/dotfiles/zsh ~/zsh
 ln -s ~/dotfiles/zsh/zlogin ~/.zlogin
 ln -s ~/dotfiles/zsh/zlogout ~/.zlogout
 ln -s ~/dotfiles/zsh/zprofile ~/.zprofile
@@ -12,10 +11,8 @@ ln -s ~/dotfiles/zsh/fzf ~/.fzf
 ln -s ~/dotfiles/zsh/fzf.zsh ~/.fzf.zsh
 
 # vim symlink
-# ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 
 # tmux symlink
-# ln -s ~/dotfiles/tmux ~/.tmux # fix weird behavior
 ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 
