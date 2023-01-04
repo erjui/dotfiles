854bc03dcdedf57daeeb686aa3a792aad1191e5f
erjui <ksj970531@gmail.com>
2023-01-04 21:35:34 +0900

    ubuntu-setup: Refactor terminal setup

--- ubuntu-setup/02_terminal_setup.sh
+++ ubuntu-setup/02_terminal_setup.sh
@@ -5,52 +5,43 @@ YELLOW='\033[0;33m'
 NC='\033[0m'
 echo -e "${RED}| ${YELLOW}03_terminal_setup.sh begin ${RED}| ${NC}\n"
 
-# terminal setting
-echo -e "Start setting terminal"
-echo -e "Install Zsh..."
-sudo apt install -y zsh
-zsh --version
+install_zsh() {
+    # install zsh
+    sudo apt install -y zsh
 
-echo -e "Install Oh My Zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# REMIND: stop at this point: press ctrl D
+    # install oh-my zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo -e "Install Zsh Theme..."
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    # install zsh theme: powerlevel10k
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+    wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+    wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+    wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+    wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
 
-sudo mkdir -p /home/seongjae/.local/share/fonts
-sudo mv MesloLGS* /home/seongjae/.local/share/fonts/
+    sudo mkdir -p /home/seongjae/.local/share/fonts
+    sudo mv MesloLGS* /home/seongjae/.local/share/fonts/
 
-fc-cache -f -v
-fc-list | grep -i MesloLGS
+    fc-cache -f -v
+    fc-list | grep -i MesloLGS
 
-echo -e "Install Zsh Plugins..."
-echo -e "Install Zsh Plugins: zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-echo -e "Install Zsh Plugins: zsh-syntax-highlighting"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-echo -e "Install Zsh Plugins: fzf"
-sudo apt-get install -y fzf
-echo -e "Install Zsh Plugins: fasd"
-sudo add-apt-repository ppa:aacebedo/fasd
-sudo apt-get update
-sudo apt-get install fasd
+    # install zsh plugins
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-echo -e "Install Guake terminal..."
-sudo apt install guake
-sudo cp -P /usr/share/applications/guake.desktop /etc/xdg/autostart/
-# REMIND: guake -p to change settings
+    # setup default shell
+    sudo chsh -s $(which zsh)
+}
 
-# Shell change done by installing Oh My Zsh #REMIND: seems not working right after installation
-echo -e "Change default shell to zsh..."
-sudo chsh -s $(which zsh)
-echo -e $SHELL
-$SHELL --version
+install_guake() {
+    # install guake
+    sudo apt install -y guake
+    sudo cp -P /usr/share/applications/guake.desktop /etc/xdg/autostart/
+}
+
+install_zsh
+install_guake
 
 echo -e "${RED}| ${YELLOW}03_terminal_setup.sh done ${RED}| ${NC}"
 echo -e "Type any keyboard input to continue...\n"
