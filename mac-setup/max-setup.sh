e1beee39f36c1c8cb132bd674b3e82e078309962
erjui <ksj970531@gmail.com>
2023-03-28 11:16:10 +0900

    mac-setup: Add simple install script

--- mac-setup/max-setup.sh
+++ mac-setup/max-setup.sh
@@ -1,18 +1,75 @@
 #!/bin/bash
 
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-export PATH=/opt/homebrew/bin:$PATH
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
 
-# Install Homebrew packages
-brew install git
-brew install wget
-brew install htop
-brew install curl 
+system_check() {
+    # system check
+    echo -e "${RED}uname -a\n${NC}`uname -a`\n"
+    echo -e "Type any keyboard input to continue...\n"
+    read
+}
 
-brew install node
+install_homebrew() {
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    export PATH=/opt/homebrew/bin:$PATH
+}
 
-brew install iterm2
-brew install --cask visual-studio-code
+install_packages() {
+    echo -e "Install Basic Packages..."
+    local packages=( \
+        curl less tree \
+        htop \
+        vim tmux \
+        tig gh \
+        bat fd-find ripgrep fzf duf \
+        direnv \
+        asciinema neofetch \
+        ncal \
+        rsync \
+        tldr \
+        git wget \
+        node \
+        iterm2 \
+        visual-studio-code \
+    )
+    # Install Homebrew packages
+    for package in ${packages[@]}; do
+        brew install $package
+    done
+}
 
-brew install tree
+set_python_symlink() {
+    # python symbolic link
+    echo -e "Set python symbolic link..."
+    sudo ln -s /usr/bin/python3 /usr/bin/python
+}
+
+install_desktop() {
+    system_check
+    install_homebrew
+    install_packages
+    install_node
+    set_python_symlink
+}
+
+case $1
+in
+    de|desktop)
+        echo -e "${RED}| ${YELLOW}mac-setup.sh desktop begin ${RED}| ${NC}\n"
+        install_desktop
+        echo -e "${RED}| ${YELLOW}mac-setup.sh desktop done ${RED}| ${NC}"
+        echo -e "Type any keyboard input to continue...\n"
+        ;;
+    h*|help|*)
+        echo "Usage: bash mac-setup.sh [OPTION]"
+        echo "Options:"
+        echo ""
+        echo "  desktop    Install linux/ubuntu for Desktop"
+        echo "  help       Print this help"
+        echo ""
+esac
+
+# todo. exa, neovim, fasd, exa, anaconda, node
